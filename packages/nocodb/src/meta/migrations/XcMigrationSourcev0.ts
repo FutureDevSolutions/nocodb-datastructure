24659e90d889deb694048ea471354efef193a954
Raju Udava <86527202+dstala@users.noreply.github.com>
2026-08-20 12:09:17 +0000

    feat(rls): add read_only access level (visible-but-locked records)

--- packages/nocodb/src/meta/migrations/XcMigrationSourcev0.ts
+++ packages/nocodb/src/meta/migrations/XcMigrationSourcev0.ts
@@ -93,6 +93,7 @@ import * as nc_202607121506_credit_system from './v0/nc_202607121506_credit_syst
 import * as nc_202608071500_ai_col_global_model_reset from './v0/nc_202608071500_ai_col_global_model_reset';
 import * as nc_202608121200_base_sections from './v0/nc_202608121200_base_sections';
 import * as nc_202608191200_credit_hold_ref_scoped from './v0/nc_202608191200_credit_hold_ref_scoped';
+import * as nc_202608201200_rls_policy_access_level from './v0/nc_202608201200_rls_policy_access_level';
 
 // Create a custom migration source class
 export default class XcMigrationSourcev0 {
@@ -197,6 +198,7 @@ export default class XcMigrationSourcev0 {
       'nc_202608071500_ai_col_global_model_reset',
       'nc_202608121200_base_sections',
       'nc_202608191200_credit_hold_ref_scoped',
+      'nc_202608201200_rls_policy_access_level',
     ]);
   }
 
@@ -396,6 +398,8 @@ export default class XcMigrationSourcev0 {
         return nc_202608121200_base_sections;
       case 'nc_202608191200_credit_hold_ref_scoped':
         return nc_202608191200_credit_hold_ref_scoped;
+      case 'nc_202608201200_rls_policy_access_level':
+        return nc_202608201200_rls_policy_access_level;
     }
   }
 }
